a767023c949ec37baaf9f5a9200a0e79800234e8
José Victor <victdiasmd@gmail.com>
2022-06-19 21:01:24 -0300

    Falta separar os componets e organizar para usar props

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,9 @@
 import React from "react";
 import grid_images from "../images/Group 77.png";
 import star from "../images/Star 1.svg";
-import image12 from "../images/image 12.png";
+import image1 from "../images/image 12.png";
+import image2 from "../images/wedding-photography 1.png";
+import image3 from "../images/mountain-bike 1.png";
 
 export default function MainContent() {
     return (
@@ -18,34 +20,52 @@ export default function MainContent() {
             </section>
             <section className="sells">
                 <div>
-                    <img src={image12} className="capa"/>
-                    <div className="avaliation-envolver">
-                        <img src={star} className="star"/>
-                        <span className="avaliation">5.0</span>
-                        <span className="other-info">(6)·USA</span>
+                    <img src={image1} className="capa" />
+                    <div className="state-container">
+                        <p className="state">sold out</p>
                     </div>
-                    <p>Life lessons with Katie Zaferes</p>
-                    <p>From $136 / person</p>
+                    <div className="information-container">
+                        <div className="avaliation-envolver">
+                            <img src={star} className="star" />
+                            <span className="avaliation">5.0</span>
+                            <span className="other-info">(6)·USA</span>
+                        </div>
+                        <p>Life lessons with Katie Zaferes</p>
+                        <p>
+                            <strong>From $136</strong> / person
+                        </p>
+                    </div>
                 </div>
                 <div>
-                    <img src={image12} className="capa"/>
-                    <div className="avaliation-envolver">
-                        <img src={star} className="star"/>
-                        <span className="avaliation">5.0</span>
-                        <span className="other-info">(6)·USA</span>
+                    <img src={image2} className="capa" />
+                    <div className="state-container">
+                        <p className="state">offline</p>
                     </div>
-                    <p>Life lessons with Katie Zaferes</p>
-                    <p>From $136 / person</p>
+                    <div className="information-container">
+                        <div className="avaliation-envolver">
+                            <img src={star} className="star" />
+                            <span className="avaliation">5.0</span>
+                            <span className="other-info">(30)·USA</span>
+                        </div>
+                        <p>Learn wedding photography</p>
+                        <p>
+                            <strong>From $125</strong> / person
+                        </p>
+                    </div>
                 </div>
                 <div>
-                    <img src={image12} className="capa"/>
-                    <div className="avaliation-envolver">
-                        <img src={star} className="star"/>
-                        <span className="avaliation">5.0</span>
-                        <span className="other-info">(6)·USA</span>
+                    <img src={image3} className="capa" />
+                    <div className="information-container">
+                        <div className="avaliation-envolver">
+                            <img src={star} className="star" />
+                            <span className="avaliation">4.8</span>
+                            <span className="other-info">(2)·USA</span>
+                        </div>
+                        <p>Group Mountain Biking</p>
+                        <p>
+                            <strong>From $50</strong> / person
+                        </p>
                     </div>
-                    <p>Life lessons with Katie Zaferes</p>
-                    <p>From $136 / person</p>
                 </div>
             </section>
         </div>
